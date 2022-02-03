f0094a89041aa1eaf39d9e24db029b731af057f3
Tim Menzies <timm@ieee.org>
2022-02-03 01:20:17 -0500

    save

--- src/lib.lisp
+++ src/lib.lisp
@@ -1,27 +1,55 @@
 ; vim: ts=2 et sw=2 et:
-(defun make () (load "lib"))
-(defun options() "
-lisp lib.lisp [OPTIONS]
-
-OPTIONS
-  :ass  asdasas   = 23
-  :help asdasassd = true
-  :b    asaasdas  = 100")
-
-
+;;;; ---------------------------------------------------------------------------
 (defmacro aif (test yes &optional no) 
   `(let ((it ,test)) (if it ,yes ,no)))
 
 (defmacro whale (expr &body body) 
   `(do ((a ,expr ,expr)) ((not a)) ,@body))
 
+(defmacro $ (x) 
+  `(opt-value (cdr (assoc *config* ,x))))
 
-(defmacro ? (p x &rest xs) 
-  (if (null xs) `(slot-value ,p ,x) `(? (slot-value ,p ,x) ,@xs)))
+(defmacro ? (s x &rest xs) 
+  (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
 
+;;;; ---------------------------------------------------------------------------
 (defun args () 
   #+clisp (coerce (EXT:ARGV) 'list) 
   #+sbcl  (cdr sb-ext:*posix-argv*))
+
+(defun opt (key help value) 
+  (labels ((cli (key value &optional (com (copy-tree (args)))) 
+                (aif (pop com)
+                  (if (eql key (ignore-errors (read-from-string it)))
+                    (cond ((equal value "true") nil)
+                          ((equal value "false") t)
+                          (t (ignore-errors (read-from-string (pop com)))))
+                    (cli key value com)))))
+    (cons key (%make-option :key key :help help :value (or (cli key value) value)))))
+
+(defun %show-option (x str depth)
+  (declare (ignore depth))
+  (format str "  :~5a  ~30a  = ~a" (string-downcase (? x key)) (? x help) (? x value)))
+
+(defstruct (option (:print-function %show-option)
+                   (:constructor %make-option)) key value help)
+
+(defun help ()
+  "sbcl --script lib.lisp [OPTIONS
+(c) 2022, Tim Menzies, MIT licence
+
+Lets have some fun.
+
+OPTIONS:"
+  (list (opt :b "asda" 23)
+        (opt :p "asda" 2)
+        (opt :k "asda" 41)
+        (opt :file "asda" "asdas")
+        (opt :q "asda" 1000)))
+
+
+;;;; ---------------------------------------------------------------------------
+(defun make () (load "lib"))
 
 (defun trim (x) 
   (string-trim '(#\Space #\Newline #\Tab) x))
@@ -46,35 +74,11 @@ OPTIONS
 (defmacro with-csv ((lst file &optional out) &body body)
   `(progn (csv ,file #'(lambda (,lst) ,@body)) ,out))
 
-;;;; ------------------------------------------------
-(defmacro $ (x) `(opt-value (cdr (assoc *config* ,x))))
 
-(defstruct (option (:print-function (lambda(x s _) (format s "1"))
-                    :constructor %make-option)) 
-  key value help)
-
-(defun make-option (key help value) 
-  (labels ((cli (key value &optional (com (copy-tree (args)))) 
-                (aif (pop com)
-                  (if (eql key (ignore-errors (read-from-string it)))
-                    (cond ((equal value "true") nil)
-                          ((equal value "false") t)
-                          (t (ignore-errors (read-from-string (pop com)))))
-                    (cli key value com)))))
-    (cons key (%make-option :key key :help help :value (or (cli key value) value)))))
-
-(defun config ()
-  (list 
-    (make-option :b "asda" 23)
-    (make-option :p "asda" 2)
-    (make-option :k "asda" 41)
-    (make-option :file "asda" "asdas")
-    (make-option :q "asda" 1000)))
-
+;;;; ---------------------------------------------------------------------------
 (defun _while(&aux (x '(1 2 3)))
   (whale (pop x) (print a)))
 
-(defun _args() (cli :aa "sad"))
 
 (defun _csv() 
   (let (head)
@@ -83,4 +87,4 @@ OPTIONS
         (format t "~s~%" (mapcar #'cell line))
         (setf head line)))))
 
-(print (config))
+(mapc #'print (mapcar #'cdr (help)))
