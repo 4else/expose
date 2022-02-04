88902dc3316aaa298b8dd4de08b73b69efd35053
Tim Menzies <timm@ieee.org>
2022-02-04 01:45:17 -0500

    save

--- src/lib.lisp
+++ src/lib.lisp
@@ -8,9 +8,36 @@ Lets have some fun.")
   (options 
     '((b    "-b" "asda" 23)
       (p    "-p" "asda" 2)
-      (help "-help" "asda" nil)
+      (help "-h" "asda" nil)
+      (license "-l" "asda" nil)
       (file "-f" "asda" "asdas")
-      (q    "-q" "asda" 1000))))
+      (seed "-s" "random number seed" 10019)
+      (todo "-t" "start up action" "")
+      (q    "-q" "asda" 1000)))
+  (copyright "
+Copyright (c) 2022 Tim Menzies
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 
 (defvar *config* (make-our))
 
@@ -21,15 +48,15 @@ Lets have some fun.")
 
 ;;;; macros --------------------------------------------------------------------
 (defmacro aif (test yes &optional no) 
-  "Anaphoric if (using `it` as the variable)."
+  "Anaphoric if (traps result of conditional in `it`)."
   `(let ((it ,test)) (if it ,yes ,no)))
 
 (defmacro whale (expr &body body) 
-  "Anaphoric while (using `a` as the variable)."
+  "Anaphoric while (traps result of conditional in `a`)."
   `(do ((a ,expr ,expr)) ((not a)) ,@body))
 
 (defmacro $ (x &optional (our *config*)) 
-  "Access a variable name."
+  "Access  a config variable name."
   `(fourth (assoc ',x (our-options ,our))))
 
 (defmacro ? (s x &rest xs) 
@@ -71,6 +98,7 @@ Lets have some fun.")
          (subseqs (or (read-line str nil) (return-from csv)))))))
 
 (defun num?(x)
+  "Return a number, if you can"
   (let ((y (ignore-errors (read-from-string x))))
     (cond ((null y) x)
           ((numberp y) y)
@@ -78,25 +106,48 @@ Lets have some fun.")
 
 (defun cli (&optional (our (make-our)) (lst (args)))
   "Maybe update `our` with data from command line."
-  (labels 
-    ((cli1 (flag x) (aif (member flag lst :test #'equalp)
-                          (cond ((equal x t)   nil) ; flip booleans
-                                ((equal x nil) t)   ; flp booleans
-                                (t (or (num? (second it)) x)))
-                          x)))
+  (labels ((cli1 (flag x) 
+                 (aif (member flag lst :test #'equalp)
+                   (cond ((equal x t)   nil) ; flip booleans
+                         ((equal x nil) t)   ; flip booleans
+                         (t (or (num? (second it)) x)))
+                   x)))
     (dolist (x (our-options our) our)
       (setf (fourth x) (cli1 (second x) (fourth x))))))
 
-(do-all-symbols(s *package*)
-  (if (equal 0 (search "_" (string s) :test #'char=)) (print s)))
+(defun randf (&optional (n 1.0)) 
+  (setf ($ seed)  (mod (* 16807.0d0 ($ seed)) 2147483647.0d0))
+  (* n (- 1.0d0 (/ ($ seed) 2147483647.0d0))))
+
+(defun randi (&optional (n 1)) 
+  (floor (* n (/ (randf 1000000.0) 1000000))))
 
 ;;;; ---------------------------------------------------------------------------
+(defvar *tests* nil)
+(defvar *fails* 0)
+
+(defmacro deftest (name params  doc  &body body)
+  `(progn (pushnew  ',name *tests*) 
+          (defun ,name ,params ,doc ,@body)))
+
+(defun demos (&optional what)
+  (dolist (one *tests*)
+    (let ((doc (documentation one 'function)))
+      (when (or (not what) (eql one what))
+        (setf *config* (cli (make-our)))
+        (multiple-value-bind (_ err)
+          (ignore-errors (funcall one))
+          (incf *fails* (if err 1 0))
+          (if err
+            (format t "~&~a [~a] ~a ~a~%" "FAIL" one doc err)
+            (format t "~&~a [~a] ~a~%"    "PASS" one doc)))))))  
+
 (defun make () (load "lib"))
 
-(defun _while(&aux (x '(1 2 3)))
+(deftest while(&aux (x '(1 2 3)))
   (whale (pop x) (print a)))
 
-(defun _csv() 
+(deftest csv() 
   (let (head)
     (with-csv (line "../data/auto93.csv") 
       (if head
@@ -105,3 +156,4 @@ Lets have some fun.")
 
 (setf *config* (cli (make-our)))
 (if ($ help) (print *config*))
+(if ($ license) (princ (our-copyright *config*)))
