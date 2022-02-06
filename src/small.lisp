002fde7f5f4a3915aab8da7237a3e519323f89f2
Tim Menzies <timm@ieee.org>
2022-02-05 22:17:19 -0500

    save

--- src/small.lisp
+++ src/small.lisp
@@ -7,7 +7,7 @@
 ; |_______/    |__|  |__| /__/     \__\ |_______||_______|
 
 (defpackage :small (:use :cl))
-(in-package :small)
+(in-package :small)
 (defstruct our 
   (help 
 "sbcl --noinform --script small.lisp [OPTIONS
@@ -112,6 +112,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 ; [__   |  |__/ | |\ | | __ [__  
 ; ___]  |  |  \ | | \| |__] ___] 
 
+(defun ucase (s)
+  (let ((s (char s 1)))
+    (equal s (string-upcase s))))
+
 (defun trim (x) 
   "Remove whitespace front and back."
   (string-trim '(#\Space #\Newline #\Tab) x))
@@ -150,6 +154,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
                             x)))
     (dolist (x (our-options our) our)
       (setf (fourth x) (cli1 (second x) (fourth x))))))
+; _  _ _ ____ ____ -------------------------------------------------------------
+; |\/| | [__  |    
+; |  | | ___] |___ 
+ (let ((_id 0)) 
+   (defun id () (incf _id)))
+
 ; ____ _  _ ____ ---------------------------------------------------------------
 ; |  | |  | |__/ 
 ; |__| |__| |  \ 
@@ -165,8 +175,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 ; /\__, `\   \ \ \_    \ \ \/     \ \ \_\ \    /\ \__/    \ \ \_     /\__, `\
 ; \/\____/    \ \__\    \ \_\      \ \____/    \ \____\    \ \__\    \/\____/
 ;  \/___/      \/__/     \/_/       \/___/      \/____/     \/__/     \/___/ 
-;                                                                            
-                                                                           
+
 ; ____ ____ _ _ _ --------------------------------------------------------------
 ; |___ |___ | | | 
 ; |    |___ |_|_| 
@@ -174,7 +183,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 (defstruct (few (:constructor %make-few)) 
   ok (n 0) (lst (make-array 5 :adjustable t :fill-pointer 0)) (max ($ enough)))
 
-(defun make-few (&key init) (adds (%make-few) init))
+(defun make-few (&key init) (adds init (%make-few)))
 
 (defmethod add1 ((f few) x)
   (with-slots (max ok lst n) f
@@ -205,7 +214,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
   (lo most-positive-fixnum) (hi most-negative-fixnum))
 
 (defun make-num (&key init (txt "") (at 0) )
-  (adds (%make-num :txt txt :at at :w (if (find #\< txt) -1 1)) init))
+  (adds init (%make-num :txt txt :at at :w (if (find #\< txt) -1 1))))
 
 (defmethod add1 ((n num) x)
   (with-slots (n lo hi all) n
@@ -214,8 +223,21 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
     (setf lo (min x lo)
           hi (max x hi))))
 
-(defmethod div ((f num)) (div (? f all)))
-(defmethod mid ((f num)) (mid (? f all)))
+(defmethod dist2 ((n num) a b)
+  (cond ((and (eq a #\?) (eq b #\?))  1)
+        ((eq a #\?) (setf b (norm n b) 
+                          a (if (> b 0.5) 1 0)))
+        ((eq b #\?) (setf a (norm n a) 
+                          b (if (> a 0.5) 1 0)))
+        (t          (setf a (norm n a) 
+                          b (norm n b))))
+  (abs (- a b)))
+
+(defmethod div ((n num)) (div (? n all)))
+(defmethod mid ((n num)) (mid (? n all)))
+(defmethod norm ((n num) x)
+  (with-slots (lo hi) n
+    (if (< (- hi lo) 1e-32) 0 (/ (- x lo)) (- hi lo))))
 ; ____ _   _ _  _ --------------------------------------------------------------
 ; [__   \_/  |\/| 
 ; ___]   |   |  | 
@@ -224,7 +246,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
   mode seen (n 0) (at 0) (txt "") (most 0))
 
 (defun make-sym (&key init (txt "") (at 0) )
-  (adds (%make-sym :txt txt :at at) init))
+  (adds init (%make-sym :txt txt :at at)))
 
 (defmethod add1 ((s sym) x)
   (with-slots (n seen most mode) s
@@ -232,6 +254,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
       (if (> now most)
         (setf most now
               mode x)))))
+
+(defmethod dist2 ((c sym) x y) (if (eql x y) 0 1))
 
 (defmethod div ((f sym)) 
   (labels ((p    (x) (/ (cdr x) (? f n)))
@@ -243,14 +267,68 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 ; | __ |    |  | |___ 
 ; |__] |___ |__| |___ 
                     
-(defun add (it x)
+(defmethod add ((it t) x)
   (unless (eq x #\?)
     (incf (? it n))
     (add1 it x))
   x)
 
-(defun adds (s lst) (dolist (new lst s) (add s new)))
-;                             __                
+(defmethod adds (lst s) 
+  (dolist (new lst s) (add s new)))
+
+(defun dist1 (col x y) 
+  (if (and (eq x #\?) (eq y #\?)) 
+      1 
+      (dist2 x y)))
+
+
+
+
+
+
+
+
+; ____ _  _ ____ _  _ ___  _    ____ -------------------------------------------
+; |___  \/  |__| |\/| |__] |    |___ 
+; |___ _/\_ |  | |  | |    |___ |___ 
+
+(defstruct example cells)
+
+(defmethod cell ((i example) (c integer))  (aref (? i cells) c))
+(defmethod cell ((i example) c)            (aref (? i cells) (? c at)))
+
+(defmethod cells ((i example)) (? i cells))
+(defmethod cells ((i cons))    i)
+
+(defmethod lt ((i example) (j example) cols) 
+  (let ((s1 0) (s2 0) (n (length cols)))
+    (dolist (col cols (< (/ s1 n) (/ s2 n)))
+      (let ((a (norm col (cell i col)))
+            (b (norm col (cell i col))))
+        (decf s1 (exp (* (? col w) (/ (- a b) n))))
+        (decf s2 (exp (* (? col w) (/ (- b a) n))))))))
+
+(defmethod dist ((i example) (j example) cols)
+  (let ((d 0) (n (length cols)))
+    (dolist (col cols (expt (/ d n) (/ 1 ($ p))))
+      (incf d (dist1 col (cell i col) (cell j col))))))
+; ____ ____ _  _ ___  _    ____ -----------------------------------------------
+; [__  |__| |\/| |__] |    |___ 
+; ___] |  | |  | |    |___ |___ 
+(defstruct sample x y rows cols)
+
+(defmethod add ((s sample) eg)
+  (with-slots (x y rows cols) s
+    (if cols
+      (push (mapcar #'add cols (cells eg)) rows)
+      (setf cols (mapcar #'(lambda (c) (col s c)) (cells eg))))
+    eg))
+
+(defmethod col ((s sample) str)
+  (let ((now (if (subseq str 0 1) o
+
+  (make-instance 'person
+;                             __                
 ;   ___ ___          __      /\_\        ___    
 ; /' __` __`\      /'__`\    \/\ \     /' _ `\  
 ; /\ \/\ \/\ \    /\ \L\.\_   \ \ \    /\ \/\ \ 
@@ -295,11 +373,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."))
 
 (dofun num.(&aux (n (make-num)))
   "streams of nums"
-  (print (has (? (adds n '(1 2 4 #\? 1 1 1 1 1 11 )) all))))
+  (print (has (? (adds '(1 2 4 #\? 1 1 1 1 1 11 ) n) all))))
 
 (dofun sym.(&aux (s (make-sym)))
   "streams of symbols"
-  (print (div (adds s (coerce "aaaabbc" 'list)))))
+  (print (div (adds (coerce "aaaabbc" 'list) s))))
 ; ____ ___ ____ ____ ___ -------------------------------------------------------
 ; [__   |  |__| |__/  |  
 ; ___]  |  |  | |  \  |  
