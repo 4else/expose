6bc6f3ef3b6b7445f5b3f59af7fcdd162d90b6b0
Tim Menzies <timm@ieee.org>
2022-02-08 15:55:41 -0500

    save

--- src/diverse.lisp
+++ src/diverse.lisp
@@ -23,40 +23,33 @@ things you might be missing.
 ## Group Exercises
 
 - Short exercise
-  - With a group of 6 to 20  friends, play "What is  privilege" game. Ask
-    the questions below, letting the questions 
-    make you step forward or backward [^zoom]. 
-  - Afterwards, form a circle and
-    watch [this video](https://www.youtube.com/watch?v=hD5f8GuNuGQ).
-  - Then, working clockwise around the circle, take turns to
-    offer 2-3 sentences on your experience with the game
-    (or say "pass" if you do not want to talk; or if what you want to
+  - With a group of 6 to 20  friends, play "What is  privilege" game. Ask the 
+    questions below, letting the questions make you step forward or backward [^zoom].
+  - Afterwards, form a circle and watch [this video](https://www.youtube.com/watch?v=hD5f8GuNuGQ).
+  - Then, working clockwise around the circle, take turns to offer 2-3 sentences on 
+    your experience with the game (or say "pass" if you do not want to talk; or if what you want to
     say has been said before).
 - Longer exercise (about a week, multiple sessions):
-   - Design tests for a system using a  _cognitive walk through_. 
-     According to [Wikipedia](https://en.wikipedia.org/wiki/Cognitive_walkthrough):
-     - The cognitive walkthrough method is a usability inspection method used to identify usability issues in interactive systems, focusing on how easy it is for new users to accomplish tasks with the system. A cognitive walkthrough is task-specific, whereas heuristic evaluation takes a holistic view to catch problems not caught by this and other usability inspection methods. 
-   - Extract _06 - CS-SE - Fall 2020 - GenderMag Exploration.docx_ from 
-     [this on-line zip](https://figshare.com/articles/online_resource/ICER_2021_Supplemental_Documents/14294744);
-   - In a group of four or more, consider some enhancment to
-     a digital phone app (for details, see the extracted document).
-   - Follow the guidelines of the extracted document.
-   - At the end, everyone write separately the tests you would propose.
-   - Finally, meet up look for differences and similarities in your proposed
+  - Design tests for a system using a  _cognitive walk through_.  According to [Wikipedia](https://en.wikipedia.org/wiki/Cognitive_walkthrough):
+    - The cognitive walkthrough method is a usability inspection method used to identify usability issues in interactive systems, focusing on how easy it is for new users to accomplish tasks with the system. A cognitive walkthrough is task-specific, whereas heuristic evaluation takes a holistic view to catch problems not caught by this and other usability inspection methods. 
+  - Extract _06 - CS-SE - Fall 2020 - GenderMag Exploration.docx_ from 
+    [this on-line zip](https://figshare.com/articles/online_resource/ICER_2021_Supplemental_Documents/14294744);
+  - In a group of four or more, consider some enhancment to
+    a digital phone app (for details, see the extracted document).
+  - Follow the guidelines of the extracted document.
+  - At the end, everyone write separately the tests you would propose.
+  - Finally, meet up look for differences and similarities in your proposed
     tests. 
 - Written report (about 2-3 weeks of work): meet as a group and 
   study 
   five of  the standard undergraduate subjects taught in a computer science
   program (second year, and higher):
-  - What harm could befall people based on the decisions
-    about the systems discussed in those classes?
+  - What harm could befall people based on the decisions about the systems discussed in those classes?
   - How to mitigate that potential harms?
-  - How to redesign the teaching of those subjects such that
-    more power is granted to the people using the systems discussed
-    in those classes.
-- Term project: answer the same "harm, mitigate, empower" questions
-  but this time, go out into the community and work with some specific
-  social group to redesign some some software that they use of a regular basis.
+  - How to redesign the teaching of those subjects such that more power is granted to 
+    the people using the systems discussed in those classes.
+- Term project: answer the same "harm, mitigate, empower" questions but this time, go out into the 
+  community and work with some specific social group to redesign some some software that they use of a regular basis.
 
 [^zoom]: Or, if you want to be virtual about it, run an on-line meeting where everyone touches their sternum. Move your hand up or down  (one finger width) according to your the answers.
 
@@ -65,39 +58,39 @@ things you might be missing.
 - Essay: as per the written report above, but only for one subject.
 
 ### 45 questions
-- If your parents worked nights and weekends to support your family, take one step back.
-- If you are able to move through the world without fear of sexual assault, take one step forward.
-- If you can show affection for your romantic partner in public without fear of ridicule or violence, take one step forward.
-- If you have ever been diagnosed as having a physical or mental illness/disability, take one step back.
-- If the primary language spoken in your household growing up was not english, take one step back.
-- If you came from a supportive family environment take one step forward.
-- If you have ever tried to change your speech or mannerisms to gain credibility, take one step back.
-- If you can go anywhere in the country, and easily find the kinds of hair products you need and/or cosmetics that match your skin color, take one step forward.
-- If you were embarrassed about your clothes or house while growing up, take one step back.
-- If you can make mistakes and not have people attribute your behavior to flaws in your racial/gender group, take one step forward.
-- If you can legally marry the person you love, regardless of where you live, take one step forward.
-- If you were born in the United States, take one step forward.
-- If you or your parents have ever gone through a divorce, take one step back.
-- If you felt like you had adequate access to healthy food growing up, take one step forward
-- If you are reasonably sure you would be hired for a job based on your ability and qualifications, take one step forward.
-- If you would never think twice about calling the police when trouble occurs, take one step forward.
-- If you can see a doctor whenever you feel the need, take one step forward.
-- If you feel comfortable being emotionally expressive/open, take one step forward.
-- If you have ever been the only person of your race/gender/socio-economic status/ sexual orientation in a classroom or workplace setting, please take one step back.
-- If you took out loans for your education take one step backward.
-- If you get time off for your religious holidays, take one step forward. 
-- If you had a job during your high school and college years, take one step back.
-- If you feel comfortable walking home alone at night, take one step forward.
-- If you have ever traveled outside the United States, take one step forward.
-- If you have ever felt like there was NOT adequate or accurate representation of your racial group, sexual orientation group, gender group, and/or disability group in the media, take one step back.
-- If you feel confident that your parents would be able to financially help/support you if you were going through a financial hardship, take one step forward.
-- If you have ever been bullied or made fun of based on something that you can’t change, take one step back.
-- If there were more than 50 books in your house growing up, take one step forward.
-- If you studied the culture or the history of your ancestors in elementary school take one step forward.
-- If your parents or guardians attended college, take one step forward.
-- If you ever went on a family vacation, take one step forward.
-- If you can buy new clothes or go out to dinner when you want to, take one step forward.
-- If you were ever offered a job because of your association with a friend or family member, take one step forward.
-- If one of your parents was ever laid off or unemployed not by choice, take one step back.
-- If you were ever uncomfortable about a joke or a statement you overheard related to your race, ethnicity, gender, appearance, or sexual orientation but felt unsafe to confront the situation, take one step back.
+1.  If your parents worked nights and weekends to support your family, take one step back.
+1.  If you are able to move through the world without fear of sexual assault, take one step forward.
+1.  If you can show affection for your romantic partner in public without fear of ridicule or violence, take one step forward.
+1.  If you have ever been diagnosed as having a physical or mental illness/disability, take one step back.
+1.  If the primary language spoken in your household growing up was not english, take one step back.
+1.  If you came from a supportive family environment take one step forward.
+1.  If you have ever tried to change your speech or mannerisms to gain credibility, take one step back.
+1.  If you can go anywhere in the country, and easily find the kinds of hair products you need and/or cosmetics that match your skin color, take one step forward.
+1.  If you were embarrassed about your clothes or house while growing up, take one step back.
+1.  If you can make mistakes and not have people attribute your behavior to flaws in your racial/gender group, take one step forward.
+1.  If you can legally marry the person you love, regardless of where you live, take one step forward.
+1.  If you were born in the United States, take one step forward.
+1.  If you or your parents have ever gone through a divorce, take one step back.
+1.  If you felt like you had adequate access to healthy food growing up, take one step forward
+1.  If you are reasonably sure you would be hired for a job based on your ability and qualifications, take one step forward.
+1.  If you would never think twice about calling the police when trouble occurs, take one step forward.
+1.  If you can see a doctor whenever you feel the need, take one step forward.
+1.  If you feel comfortable being emotionally expressive/open, take one step forward.
+1.  If you have ever been the only person of your race/gender/socio-economic status/ sexual orientation in a classroom or workplace setting, please take one step back.
+1.  If you took out loans for your education take one step backward.
+1.  If you get time off for your religious holidays, take one step forward. 
+1.  If you had a job during your high school and college years, take one step back.
+1.  If you feel comfortable walking home alone at night, take one step forward.
+1.  If you have ever traveled outside the United States, take one step forward.
+1.  If you have ever felt like there was NOT adequate or accurate representation of your racial group, sexual orientation group, gender group, and/or disability group in the media, take one step back.
+1.  If you feel confident that your parents would be able to financially help/support you if you were going through a financial hardship, take one step forward.
+1.  If you have ever been bullied or made fun of based on something that you can’t change, take one step back.
+1.  If there were more than 50 books in your house growing up, take one step forward.
+1.  If you studied the culture or the history of your ancestors in elementary school take one step forward.
+1.  If your parents or guardians attended college, take one step forward.
+1.  If you ever went on a family vacation, take one step forward.
+1.  If you can buy new clothes or go out to dinner when you want to, take one step forward.
+1.  If you were ever offered a job because of your association with a friend or family member, take one step forward.
+1.  If one of your parents was ever laid off or unemployed not by choice, take one step back.
+1.  If you were ever uncomfortable about a joke or a statement you overheard related to your race, ethnicity, gender, appearance, or sexual orientation but felt unsafe to confront the situation, take one step back.
 
