35d2ffb401c0d08f05e5d079640787d12bd1228d
Tim Menzies <timm@users.noreply.github.com>
2022-02-10 00:47:14 -0500

    save

---
+++ overview.lisp
@@ -0,0 +1,25 @@
+|#
+
+starting with nothig
+we define how t coerce a string to a simple item
+
+simpel item can live in items
+
+and example is an item (perhaps with some other ifnroamtion)
+
+examples live in samples. columns of samples divide into x,y, all (and something is not ix x,y if we are ignorning it)
+
+files of data generate different coumns and examples
+
+using the x-values
+- exapels divide into clusters
+- clsuters have ranges
+-  some ranges have contrasting values (different freqeuences in one cluster than another)
+- trees are generating by recursiveng dividing n the strongest contast.
+
+using the y-vaues, we can rank sub-clsuters, perhaps infnrng sum. 
+- planning means:
+    - build the tree
+    - walk the tree, selecting best cluster
+    - benachmarking: find best leaf call it best
+#|
